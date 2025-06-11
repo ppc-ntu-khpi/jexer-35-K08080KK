5165c1d94735021c1d81cf8c221f90971a0bb28a
Ponomarenko Roman <romaponomarenko152@gmail.com>
2025-06-11 12:24:22 +0300

    ready

--- Lab 1 -TUI/TUIdemo.java
+++ Lab 1 -TUI/TUIdemo.java
@@ -1,4 +1,4 @@
-package com.mybank.tui;
+package domain;
 
 import jexer.TAction;
 import jexer.TApplication;
@@ -7,7 +7,14 @@ import jexer.TText;
 import jexer.TWindow;
 import jexer.event.TMenuEvent;
 import jexer.menu.TMenu;
-
+import com.mybank.domain.Bank;
+import com.mybank.domain.Customer;
+import com.mybank.domain.Account;
+import com.mybank.domain.CheckingAccount;
+import com.mybank.domain.SavingsAccount;
+import com.mybank.domain.*;
+import com.mybank.data.DataSource;
+import java.io.IOException;
 /**
  *
  * @author Alexander 'Taurus' Babich
@@ -60,19 +67,45 @@ public class TUIdemo extends TApplication {
     }
 
     private void ShowCustomerDetails() {
+        
+        Bank bank = Bank.getBank();
+        try {
+            DataSource dataSource = new DataSource("data/test.dat");
+            dataSource.loadData();
+        } catch (IOException e) {
+            System.err.println("Error loading data: " + e.getMessage());
+            return;
+        }
+
         TWindow custWin = addWindow("Customer Window", 2, 1, 40, 10, TWindow.NOZOOMBOX);
         custWin.newStatusBar("Enter valid customer number and press Show...");
 
         custWin.addLabel("Enter customer number: ", 2, 2);
         TField custNo = custWin.addField(24, 2, 3, false);
         TText details = custWin.addText("Owner Name: \nAccount Type: \nAccount Balance: ", 2, 4, 38, 8);
+
         custWin.addButton("&Show", 28, 2, new TAction() {
             @Override
             public void DO() {
                 try {
                     int custNum = Integer.parseInt(custNo.getText());
-                    //details about customer with index==custNum
-                    details.setText("Owner Name: John Doe (id="+custNum+")\nAccount Type: 'Checking'\nAccount Balance: $200.00");
+                    
+                    if (custNum < 0 || custNum >= bank.getNumberOfCustomers()) {
+                        throw new IndexOutOfBoundsException();
+                    }
+
+                    Customer customer = bank.getCustomer(custNum);
+                    Account account = customer.getAccount(0);
+                    
+                    String accountType = account instanceof CheckingAccount ? "Checking" : 
+                                        account instanceof SavingsAccount ? "Savings" : "Unknown";
+                    
+                    String text = "Owner Name: " + customer.getFirstName() + " " + customer.getLastName() +
+                                " (id=" + custNum + ")\nAccount Type: '" + accountType + "'" +
+                                "\nAccount Balance: $" + String.format("%.2f", account.getBalance());
+
+                    details.setText(text);
+
                 } catch (Exception e) {
                     messageBox("Error", "You must provide a valid customer number!").show();
                 }
